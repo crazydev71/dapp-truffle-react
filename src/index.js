fa34e6848dd3396896081983d4417fef2dc543b6
geek96 <geek96@outlook.com>
2017-07-17 16:35:28 +0500

    fix for windows

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.css';
-import App from 'app/App';
+import App from './app/App';
 import registerServiceWorker from './utils/registerServiceWorker';
 
 
