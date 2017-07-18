d292f0cde6f645096791598260ecb0c4dfffdaff
Arnoldo Mora <a3@3vot.com>
2017-07-18 15:46:09 -0600

    truffle box integrated

--- README.md
+++
@@ -1,2 +0,0 @@
-# DAO
-## DAO with governance
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,14 +1,13 @@
+var SimpleStorage = artifacts.require("./SimpleStorage.sol");
 const HelloWorld = artifacts.require("./HelloWorld.sol");
 const DAO = artifacts.require("./DAO.sol");
 const Token = artifacts.require("./Token.sol");
 const TokenCreation = artifacts.require("./TokenCreation.sol");
 
-
 module.exports = function(deployer) {
+  deployer.deploy(SimpleStorage);
   deployer.deploy(HelloWorld);
   //deployer.deploy(DAO);
   //deployer.deploy(Token);
   //deployer.deploy(TokenCreation);
 };
-
-
