6d401724d4628c572233d8b5d632ae5a162bab44
HatimKH <h.khomsi@gmail.com>
2017-07-19 20:43:39 +0100

    add ERC20 Token Contract

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,13 +1,7 @@
+var FixedSupplyToken = artifacts.require("./FixedSupplyToken.sol");
 var SimpleStorage = artifacts.require("./SimpleStorage.sol");
-const HelloWorld = artifacts.require("./HelloWorld.sol");
-const DAO = artifacts.require("./DAO.sol");
-const Token = artifacts.require("./Token.sol");
-const TokenCreation = artifacts.require("./TokenCreation.sol");
 
 module.exports = function(deployer) {
+  deployer.deploy(FixedSupplyToken);
   deployer.deploy(SimpleStorage);
-  deployer.deploy(HelloWorld);
-  //deployer.deploy(DAO);
-  //deployer.deploy(Token);
-  //deployer.deploy(TokenCreation);
 };
