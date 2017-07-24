a916270832a4a71437dff8bbca39795caa833fdf
HatimKH <h.khomsi@gmail.com>
2017-07-24 13:42:36 +0100

    Allocate total supply to the owner balance

--- test/token.js
+++ test/token.js
@@ -8,4 +8,11 @@ contract('Token', function(accounts) {
       assert.equal(totalSupply, 100000000, "100000000 wasn't in the first account");
     });
   });
+  it("should put 100 Millions Token in the owner account", function() {
+    return Token.deployed().then(function(instance) {
+      return instance.balanceOf.call(accounts[0]);
+    }).then(function(balance) {
+      assert.equal(balance.valueOf(), 100000000, "100000000 wasn't in the first account");
+    });
+  });
 });
