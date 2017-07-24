dbb08d85c0fd0860f64ea03c49c2e827d04889ef
HatimKH <h.khomsi@gmail.com>
2017-07-24 16:20:15 +0100

    Add Token transfer test

--- test/token.js
+++ test/token.js
@@ -15,12 +15,39 @@ contract('Token', function(accounts) {
       assert.equal(balance.valueOf(), 100000000, "100000000 wasn't in the first account");
     });
   });
-  it("should transfer 100 token from owner account to the second account", function() {
+  it("should transfer Token correctly", function() {
+    var meta;
+
+    // Get initial balances of first and second account.
+    var account_one = accounts[0];
+    var account_two = accounts[1];
+
+    var account_one_starting_balance;
+    var account_two_starting_balance;
+    var account_one_ending_balance;
+    var account_two_ending_balance;
+
+    var amount = 10;
+
     return Token.deployed().then(function(instance) {
-      return instance.transfer.call(accounts[1], 100);
-    }).then(function(result) {
-      // console.log("Here is the result.........", result)
-      assert.equal(result, true, "100 token wasn't transfereed from owner account to the second account");
+      meta = instance;
+      return meta.balanceOf.call(account_one);
+    }).then(function(balance) {
+      account_one_starting_balance = balance.toNumber();
+      return meta.balanceOf.call(account_two);
+    }).then(function(balance) {
+      account_two_starting_balance = balance.toNumber();
+      return meta.transfer(account_two, amount, {from: account_one});
+    }).then(function() {
+      return meta.balanceOf.call(account_one);
+    }).then(function(balance) {
+      account_one_ending_balance = balance.toNumber();
+      return meta.balanceOf.call(account_two);
+    }).then(function(balance) {
+      account_two_ending_balance = balance.toNumber();
+
+      assert.equal(account_one_ending_balance, account_one_starting_balance - amount, "Amount wasn't correctly taken from the sender");
+      assert.equal(account_two_ending_balance, account_two_starting_balance + amount, "Amount wasn't correctly sent to the receiver");
     });
   });
 });
