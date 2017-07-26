39d6d63c61a23780551d5040c817de2010e4855e
HatimKH <h.khomsi@gmail.com>
2017-07-26 15:07:08 +0100

    migrate getweb3

--- src/actions/blockchain.js
+++ src/actions/blockchain.js
@@ -1,5 +1,6 @@
 import Web3 from 'web3'
 import store from '../index'
+import { loadUser } from '../reducers/userThunks.js'
 
 // export const RESET_ERROR_MESSAGE = 'RESET_ERROR_MESSAGE'
 
@@ -13,6 +14,18 @@ export function initiateWeb3() {
         resolve(web3)
     }).catch(function(web3){
             store.dispatch(connectWeb3(web3.eth))
+
+            web3.eth.getAccounts((error, accounts) => {
+                
+                const defaultUser = {
+                    email: 'arnoldomora79@gmail.com',
+                    password: '1234',
+                    accountAddress: accounts[0]
+                    }
+
+                store.dispatch(loadUser(defaultUser))
+
+            })
     });
 }
 
