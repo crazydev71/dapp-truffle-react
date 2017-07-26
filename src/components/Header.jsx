f15f989a6c40ae1ee371ef26490555aff878abf4
HatimKH <h.khomsi@gmail.com>
2017-07-26 14:59:33 +0100

    Normalize Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,44 +1,10 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Navbar, Nav, NavItem } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 import { LinkContainer } from 'react-router-bootstrap'
 
-// import { initiateWeb3 } from '../actions/blockchain'
 
-// component
-
-class Header extends Component {
-    // componentWillMount(){
-    //     console.log('test')
-    //     initiateWeb3()
-    // }
-    render(){
-        return (
-        <Navbar inverse>
-            <Navbar.Header>
-                <Navbar.Brand>
-                    <Link to="/">DAO as a Service</Link>
-                </Navbar.Brand>
-                <Navbar.Toggle />
-            </Navbar.Header>
-            <Navbar.Collapse>
-                <Nav>
-                  <LinkContainer to="/">
-                    <NavItem>DAO Admin</NavItem>
-                  </LinkContainer>
-                    <LinkContainer to="/token">
-                    <NavItem>Old Example</NavItem>
-                  </LinkContainer>
-                </Nav>
-            </Navbar.Collapse>
-        </Navbar>
-        )
-    }
-}
-
-export default Header
-
-/*export default function Header() {
+export default function Header() {
     return (
         <Navbar inverse>
             <Navbar.Header>
@@ -59,4 +25,4 @@ export default Header
             </Navbar.Collapse>
         </Navbar>
     )
-}*/
+}
