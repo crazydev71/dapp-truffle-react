e324e4da6567490ed649d9b2e61130e36436f164
HatimKH <h.khomsi@gmail.com>
2017-07-27 20:37:23 +0100

    update

--- src/actions/dao.js
+++ src/actions/dao.js
@@ -1,32 +1,6 @@
 import store from '../index'
 
-
 export const ADD_DAO_CONTRACT = 'ADD_DAO_CONTRACT'
-
-
-// export function addContractService(name, contract, provider) {
-//     let contractInstance
-//     return new Promise(function(reject, resolve){
-//         contract.setProvider(provider)
-//         resolve(contract)
-//     }).catch(function(contract){
-//         contract.deployed().then(function(instance){
-//             console.log('tokenInstance: ', instance)
-//             contractInstance = instance
-//             return instance.totalSupply()
-//         }).then(function(result){
-//             console.log('total supply: ', result.c[0])
-//             console.log("here instance", contractInstance)
-//             addContractServiceAction({
-//                 name,
-//                 instance: contractInstance,
-//                 totalSupply: result.c[0],
-//                 address: contractInstance.address
-//             })
-//         })
-//     });
-// }
-
 export function addDaoContractAction(contract){
     store.dispatch({
         type: ADD_DAO_CONTRACT,
--- src/actions/token.js
+++ src/actions/token.js
@@ -2,31 +2,6 @@ import store from '../index'
 
 
 export const ADD_TOKEN_CONTRACT = 'ADD_TOKEN_CONTRACT'
-
-
-// export function addContractService(name, contract, provider) {
-//     let contractInstance
-//     return new Promise(function(reject, resolve){
-//         contract.setProvider(provider)
-//         resolve(contract)
-//     }).catch(function(contract){
-//         contract.deployed().then(function(instance){
-//             console.log('tokenInstance: ', instance)
-//             contractInstance = instance
-//             return instance.totalSupply()
-//         }).then(function(result){
-//             console.log('total supply: ', result.c[0])
-//             console.log("here instance", contractInstance)
-//             addContractServiceAction({
-//                 name,
-//                 instance: contractInstance,
-//                 totalSupply: result.c[0],
-//                 address: contractInstance.address
-//             })
-//         })
-//     });
-// }
-
 export function addContractServiceAction(contract){
     store.dispatch({
         type: ADD_TOKEN_CONTRACT,
