e68bb39e272a76ad46991dd69d44ca0f65a71192
HatimKH <h.khomsi@gmail.com>
2017-07-27 16:12:18 +0100

    Transfer Event function

--- src/containers/TransferContainer.js
+++ src/containers/TransferContainer.js
@@ -37,18 +37,6 @@ const MapDispatchToProps = (dispatch) => {
 class TransferContainer extends Component {
   constructor(props) {
     super(props)
-
-    // this.state = {
-    //   // web3: null,
-    //   // token: {address: 'something'},
-    //   // totalSupply: 0,
-    //   // transferEvent: null,
-    //   // approvalEvent: null,
-    //   // stop: false
-    // }
-
-    // this.transferListener = this.transferListener.bind(this)
-    // this.approvalListener = this.approvalListener.bind(this)
     this.transferToken = this.transferToken.bind(this)
   }
 
@@ -59,117 +47,11 @@ class TransferContainer extends Component {
     token.transfer(secondaryAccount, 100, {
             from: ownerAccount
           }).then(function(results){
-            console.log("RESULT_______________________", results)
+            console.log("RESULT_______________________", results.logs[0].event)
+          }).catch(function(e){
+            console.log("Exception...............")
           })
   }
-
-  //callback for Transfer Event
-  // transferListener (error, res) {
-  //   console.log('transferListener reporting: ', res)
-  //   this.state.transferEvent.stopWatching()
-  //   if(this.state.stop === false) {
-  //     //send approval transaction
-  //     this.state.token.approve.sendTransaction(res.args._to , 15000, {
-  //       from: res.args._from
-  //     })
-  //     .then(result => {
-  //       console.log('approve result: ', result)
-  //       //set approval event listener
-  //       this.setState({approvalEvent: this.state.token.Approval()})
-  //       this.state.approvalEvent.watch(this.approvalListener)
-  //     })
-  //   } else {
-  //     console.log('transferListener reporting: ', res)
-  //     this.state.token.allowance(res.args._from, res.args._to)
-  //     .then(result => {
-  //       console.log('allowance remaingin: ', result)
-  //     })
-  //   }
-  // }
-
-  // //callback for Approval Event
-  // approvalListener (error, res) {
-  //   console.log('approveListener reporting: ', res)
-  //   this.state.approvalEvent.stopWatching()
-  //   this.state.token.transferFrom.sendTransaction(res.args._owner, res.args._spender, 12000, {
-  //       from: res.args._owner
-  //     })
-  //   .then(result => {
-  //     console.log('transferFrom result',result)
-  //     //set approval event listener again
-  //     // this.setState({stop: true})
-  //     // this.setState({transferEvent: this.state.token.Transfer()})
-  //     // this.state.transferEvent.watch(this.transferListener)
-  //   })
-  // }
-
-
-  //runs when the component will mount for first time
-  // componentWillMount() {
-  // // Get network provider and web3 instance.
-  // // See utils/getWeb3 for more info.
-  // // will be moved to a reducer later
-
-  // // getWeb3
-  // //   .then(results => {
-  // //     console.log('Inserting web3 results:',results)
-  // //     this.setState({web3: this.props.web3})
-  // //     this.instantiateContract()
-  // //   })
-  // //   .catch(() => {
-  // //     console.log('Error finding web3.')
-  // //   })
-  // }
-
-  // //Token contract instance
-  // instantiateContract() {
-
-  //   const contract = require('truffle-contract')
-  //   const token = contract(TokenContract)
-  //   token.setProvider(this.state.currentProvider)
-
-  //   // Declaring this for later so we can chain functions on Token.
-  //   let tokenInstance
-  //   let accountsList
-
-  //   // Get accounts.
-  //   this.state.web3.getAccounts((error, accounts) => {
-  //       accountsList = accounts
-  //       console.log('account 0: ',accountsList[0])
-  //       token.deployed().then((instance) => {
-  //         tokenInstance = instance
-  //         //set token to state
-  //         this.setState({ token: tokenInstance })
-  //         //
-  //         console.log('tokenInstance: ',tokenInstance)
-  //         //get token total supply
-  //         return tokenInstance.totalSupply()
-  //       })
-  //       .then((result) => {
-  //         console.log('total supply: ', result.c[0])
-  //         this.setState({ totalSupply: result.c[0] })
-  //         //get token balance
-  //         return tokenInstance.balanceOf(accountsList[0])
-  //       })
-  //       .then((result) => {
-  //         console.log('balance: ', result.c[0])
-  //           //token transfer transaction
-  //           return tokenInstance.transfer.sendTransaction(accountsList[1], 50000, {
-  //             from: accountsList[0]
-  //           })
-  //       })
-  //       .then(result => {
-  //         console.log('transfer result: ', result)
-  //         //set transfer event listener
-  //         this.setState({transferEvent: tokenInstance.Transfer()})
-  //         this.state.transferEvent.watch(this.transferListener)
-  //       })
-  //       .catch(error => {
-  //         console.error('error: ', error)
-  //       })
-
-  //     })
-  // }
 
 
 
