e89f122655534a817547738b9461db3917b19981
HatimKH <h.khomsi@gmail.com>
2017-07-29 02:03:03 +0100

    mocha test environement

--- src/reducers/blockchain.test.js
+++ src/reducers/blockchain.test.js
@@ -10,16 +10,20 @@ const provider = new Web3.providers.HttpProvider('http://localhost:8545')
 
 const web3 = new Web3(provider)
 
-describe('Web3 Reducer dispatches actions correctly', () => {
+let account
+web3.eth.getAccounts((error, accounts) => {
+  account = accounts[0]
+  describe('Web3 Reducer dispatches actions correctly', () => {
 
-  it('it should load web3', () => {
-    dispatch(web3Connect({
-    connected: web3.isCoonected(),
-    currentProvider: web3.currentProvider,
-    accounts: web3.eth.accounts,
-    web3_Ethereum: web3.eth
-    }))
-    expect(getState().blockchain.currentProvider.host).to.equal("http://localhost:8545")
+    it('it should load web3', () => {
+      dispatch(web3Connect({
+      connected: web3.isCoonected(),
+      currentProvider: web3.currentProvider,
+      accounts: web3.eth.accounts,
+      web3_Ethereum: web3.eth
+      }))
+      expect(getState().blockchain.accounts[0]).to.equal(account)
+    })
+
   })
-
 })
