99c2f2e0346477abb1e4847e7569480490f61e1f
HatimKH <h.khomsi@gmail.com>
2017-07-29 01:27:01 +0100

    files not used

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,7 +4,6 @@ import { combineReducers } from 'redux'
 //combine reducers in a single one
 const rootReducer = combineReducers({
   routerReducer,
-  web3: require('./web3').default,
   user: require('./user').default,
   masterContracts: require('./masterContracts').default,
   myContracts: require('./myContracts').default,
--- src/reducers/web3Thunks.js
+++
@@ -1,9 +0,0 @@
-import { addWeb3 } from './web3'
-
-//thunks
-
-//dispatch web3 to state in order to have it available all around the app
-export const loadWeb3 = (web3) =>
-  (disptach) => {
-    disptach(addWeb3(web3))
-  }
