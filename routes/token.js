6a4bb5da0a074cd84180d1a0dd4ff115ff7135f1
HatimKH <h.khomsi@gmail.com>
2017-10-14 00:18:52 +0100

    Token approve

--- routes/token.js
+++ routes/token.js
@@ -54,7 +54,7 @@ router.route('/transfer-token').post( (req, res) => {
 
   // Data post example:
   // {
-  //   "sharesAddress": "0x3683da91c8e7d1e022c8f18a3813db550eece8e0",
+  //   "sharesAddress": "0x27074fbb5f2740b029d88b8b3188b8c5e3d696fb",
   //   "_to" : "0xead02d42efe27ff850e242892577336de3d20cf9",
   //   "_amount" : "1",
   //   "account" : "0x7ecf34ed29ede66ecc1068b398102aa57ccbd317"
@@ -73,5 +73,32 @@ router.route('/transfer-token').post( (req, res) => {
     }).then( (result) => { return res.json( { event: result.logs[0].event } ); } );
 
 });
+
+router.route('/approve').post( (req, res) => {
+
+  // Data post example:
+  // {
+  //   "sharesAddress": "0x27074fbb5f2740b029d88b8b3188b8c5e3d696fb",
+  //   "_spender" : "0xcfd980c4115825d6270d840cab900e15a65e3436",
+  //   "_amount" : "1",
+  //   "account" : "0x7ecf34ed29ede66ecc1068b398102aa57ccbd317"
+  // }
+
+  const data = req.body;
+
+  token.at(data.sharesAddress).approve(
+    data._spender,
+    data._amount,
+    {
+      from: data.account,
+      gas: 4388712,
+      gasPrice: 100000000000
+    }).then( (result) => {
+      // console.log("event", result);
+      return res.json( { event: result.logs[0].event } ); } );
+
+});
+
+
 
 export default router;
